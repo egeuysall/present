7ef1f3ee9a9483bc055d03dda2bc78344f9ee305
Ege YourLastName <hello@egeuysal.com>
2025-06-12 21:40:51 +0300

    Set up project

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/egeuysall/present
+
+go 1.24.4
---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "@/styles/globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Present: Plan. Track. Gift.",
+  description: "Present helps you organize, track, and share gift lists with ease. Stay on budget and never miss a gift. Start managing your gifts today!",
+    icons: {
+        icon: "/icon.ico",
+    },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
