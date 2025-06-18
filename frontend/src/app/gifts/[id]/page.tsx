c69034674f38582ac688f5a7b3e0544fc9d691ad
Ege YourLastName <hello@egeuysal.com>
2025-06-18 17:38:40 +0300

    Set up project

--- frontend/src/app/gifts/[id]/page.tsx
+++ frontend/src/app/gifts/[id]/page.tsx
@@ -1,7 +1,4 @@
-import React from "react";
 import {notFound} from "next/navigation";
-import {cookies} from "next/headers";
-import DynamicGift from "@/components/dynamic-gift";
 
 type GiftType = {
     id: string;
@@ -9,50 +6,23 @@ type GiftType = {
     price: number;
 };
 
-interface DynamicGiftProps {
-    params: Promise<{ id: string }>;
-}
-
-const DynamicPage: React.FC<DynamicGiftProps> = async ({ params }) => {
-    const { id } = await params;
-
-    const cookieStore = await cookies()
-    const accessToken = cookieStore.get('access_token')
-
-    if (accessToken?.value) {
-        cookieStore.set({
-            name: 'access_token',
-            value: accessToken?.value,
-            httpOnly: true,
-            secure: true,
-            sameSite: 'strict',
-            path: `/gifts/${id}`
-        })
-    }
-
+export async function generateStaticParams() {
     try {
-        const res = await fetch(
-            `https://presentapi.egeuysal.com/v1/gifts/${encodeURIComponent(id)}`);
+        const res = await fetch(`https://presentapi.egeuysal.com/v1/gifts`);
 
         if (!res.ok) {
-            const text = await res.text();
-            console.error("Failed to fetch gift", {
-                id,
-                status: res.status,
-                statusText: res.statusText,
-                responseText: text,
-            });
+            console.error("Failed to fetch gifts");
             notFound();
         }
 
         const json = await res.json();
-        const gift: GiftType = json.data;
+        const gifts: GiftType[] = json.data;
 
-        return <DynamicGift gift={gift} />;
+        return gifts.map((gift) => ({
+            id: gift.id,
+        }));
     } catch (err) {
         console.error("Unexpected error during fetch", err);
         notFound();
     }
-};
-
-export default DynamicPage;
+}
